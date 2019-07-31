e0fe6407cd59bac7a1b9f4554d724a1bbadcd03d
Aubrey Malabie <malengatiger@gmail.com>
2019-07-31 22:48:18 +0200

    🥬 🥬 🥬 initial commit 🥬 🥬 🥬

---
+++ bfn-corda/contracts/src/main/java/com/template/states/SupplierState.java
@@ -0,0 +1,72 @@
+package com.template.states;
+
+import com.template.contracts.SupplierContract;
+import net.corda.core.contracts.BelongsToContract;
+import net.corda.core.contracts.ContractState;
+import net.corda.core.identity.AbstractParty;
+import net.corda.core.identity.Party;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+
+// *********
+// * State *
+// *********
+@BelongsToContract(SupplierContract.class)
+public class SupplierState implements ContractState {
+
+    private String name, email, cellphone, fcmToken;
+    private List<String> sectors;
+    private Date dateRegistered;
+    private Party party;
+
+    public SupplierState(Party party,  String name, String email, String cellphone, String fcmToken, List<String> sectors) {
+        this.name = name;
+        this.email = email;
+        this.cellphone = cellphone;
+        this.fcmToken = fcmToken;
+        this.sectors = sectors;
+        this.party = party;
+    }
+
+    public Party getParty() {
+        return party;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<String> getSectors() {
+        return sectors;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getCellphone() {
+        return cellphone;
+    }
+
+    public String getFcmToken() {
+        return fcmToken;
+    }
+
+    public Date getDateRegistered() {
+        return dateRegistered;
+    }
+
+    public void setDateRegistered(Date dateRegistered) {
+        this.dateRegistered = dateRegistered;
+    }
+
+    @Override
+    public List<AbstractParty> getParticipants() {
+        List<AbstractParty> list = new ArrayList<>();
+
+        return list;
+    }
+}
